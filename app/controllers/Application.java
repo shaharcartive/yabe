da9f2cf4a5c2b0b2a7caa1612cab94c0d04d7f97
shaharcartive <shahar@shahar-cohens-macbook-pro.local>
2015-05-20 13:06:21 +0200

    delete yabe

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -1,18 +1,20 @@
 package controllers;
-
+ 
+import java.util.*;
+ 
 import play.*;
 import play.mvc.*;
-
-import java.util.*;
-
+ 
 import models.*;
-
+ 
 public class Application extends Controller {
-
-	public static void index() {
-	    System.out.println("Yop");
-	    render();
-	}
-    
-
+ 
+    public static void index() {
+        Post frontPost = Post.find("order by postedAt desc").first();
+        List<Post> olderPosts = Post.find(
+            "order by postedAt desc"
+        ).from(1).fetch(10);
+        render(frontPost, olderPosts);
+    }
+ 
 }
